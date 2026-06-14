99c44ed56094da4f94116340c30e89d173aee1d0
Ignotum77 <258382800+Ignotum77@users.noreply.github.com>
2026-06-14 15:06:35 +0200

    Update background.js

--- background.js
+++ background.js
@@ -35,21 +35,4 @@ function Reload() {
       chrome.tabs.reload(tab.id)
     }
   });
-};"use strict";
-
-chrome.runtime.onInstalled.addListener(() => {
-  Reload();
-});
-
-function Reload() {
-  chrome.tabs.query({
-    url: [
-      "https://www.youtube.com/*",
-      "https://music.youtube.com/*"
-    ]
-  }, (tabs) => {
-    for(let tab of tabs) {
-      chrome.tabs.reload(tab.id)
-    }
-  });
 };
