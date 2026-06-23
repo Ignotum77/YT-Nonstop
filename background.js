d9998ef7cf82b1c5dd63c76b5c278c1a83c9dc4b
Ignotum77 <258382800+Ignotum77@users.noreply.github.com>
2026-06-23 16:08:33 +0200

    Add support for YT on mobile (#7)
    1. Added support for m.youtube.com on smartphone browser
    2. Converted chrome.... to browser...., since this addon is only for FireFox.
    like: `chrome.storage.sync.get(null, function(data) {` to `browser.storage.sync.get(null).then((data) => {`

--- background.js
+++ background.js
@@ -1,18 +1,48 @@
 "use strict";
 
-chrome.runtime.onInstalled.addListener(() => {
-  Reload();
-});
-
+// Manage browserAction popup display
+function isYouTube(url) {
+    return /^https:\/\/(?:www|music|m)\.youtube\.com\//.test(url);
+}
+function updateBrowserAction(tabId, url) {
+  if (isYouTube(url)) {
+    browser.browserAction.enable(tabId);
+  } else {
+    browser.browserAction.disable(tabId);
+  }
+}
+// Reload YT tabs on install to let the autonav on/off work
 function Reload() {
-  chrome.tabs.query({
-    url: [
-      "https://www.youtube.com/*",
-      "https://music.youtube.com/*"
-    ]
-  }, (tabs) => {
-    for(let tab of tabs) {
-      chrome.tabs.reload(tab.id)
+  browser.tabs.query({}).then((tabs) => {
+    for (const tab of tabs) {
+      if (tab.url && isYouTube(tab.url)) {
+        browser.tabs.reload(tab.id);
+      }
     }
   });
-};
+}
+
+browser.runtime.onInstalled.addListener(() => {
+  Reload();
+  browser.tabs.query({}).then((tabs) => {
+    for (const tab of tabs) {
+      updateBrowserAction(tab.id, tab.url || "");
+    }
+  });
+});
+browser.runtime.onStartup.addListener(() => {
+  browser.tabs.query({}).then((tabs) => {
+    for (const tab of tabs) {
+      updateBrowserAction(tab.id, tab.url || "");
+    }
+  });
+});
+browser.tabs.onActivated.addListener(async ({ tabId }) => {
+  const tab = await browser.tabs.get(tabId);
+  updateBrowserAction(tab.id, tab.url || "");
+});
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url) {
+    updateBrowserAction(tabId, tab.url);
+  }
+});
--- popup.js
+++ popup.js
@@ -1,49 +1,51 @@
 window.onload = () => {
-    document.getElementById('autoskip-toggle').addEventListener("click", function(event) {
-        //native html input element toggles on click
-        setAutoTubeListeners('autoSkip');
-    });
-    setSettings([
-        {key: 'autoSkip', cb: setAutoSkip},
-    ]);
+  document.getElementById('autoskip-toggle').addEventListener("click", function(event) {
+    //native html input element toggles on click
+    setAutoTubeListeners('autoSkip');
+  });
+  setSettings([
+    {key: 'autoSkip', cb: setAutoSkip},
+  ]);
 }; // end window.onload
 
 // helpers
 function setSettings(items){
-    chrome.storage.sync.get(null, function(data) {
-        const setItems = {};
-        items.forEach( ({key, cb}) => {
-            if (data === undefined || data[key] === undefined || data[key] === null) {
-                setItems.key = cb(true);
-            } else if(!data[key]) {
-                cb(false);
-            } else {
-                cb(true);
-            }
-        });
-        // update items if they were undefined or null
-        Object.keys(setItems).length > 0 && chrome.storage.sync.set(setItems, function() {});
+  browser.storage.sync.get(null).then((data) => {
+    const setItems = {};
+    items.forEach( ({key, cb}) => {
+      if (data === undefined || data[key] === undefined || data[key] === null) {
+        setItems[key] = cb(true);
+      } else if(!data[key]) {
+        cb(false);
+      } else {
+        cb(true);
+      }
     });
+    // update items if they were undefined or null
+    if (Object.keys(setItems).length > 0) {
+      browser.storage.sync.set(setItems);
+    }
+  });
 }
 
 function setAutoTubeListeners(key) {
-    const value = {
-        autoSkip: document.getElementById('autoskip-toggle').checked,
-    }[key];
-    chrome.tabs.query({
-      url: [
-        "https://www.youtube.com/*",
-        "https://music.youtube.com/*"
-      ]
-    },
-    (tabs) => {
-      for (let tab of tabs) {
-        chrome.tabs.sendMessage(tab.id, {[key]: value});
-      }
-    });
-    chrome.storage.sync.set({[key]: value});
+  const value = {
+    autoSkip: document.getElementById('autoskip-toggle').checked,
+  }[key];
+  browser.tabs.query({
+    url: [
+      "https://www.youtube.com/*",
+      "https://music.youtube.com/*",
+      "https://m.youtube.com/*"
+    ]
+  }).then((tabs) => {
+    for (const tab of tabs) {
+      browser.tabs.sendMessage(tab.id, {[key]: value}).catch(() => {});
+    }
+  });
+  browser.storage.sync.set({[key]: value});
 }
 
 function setAutoSkip(status) {
-    return document.getElementById('autoskip-toggle').toggleAttribute('checked', status);
+  return document.getElementById('autoskip-toggle').toggleAttribute('checked', status);
 }
